19c32f9bb0ef8cc8039e86ad5a4daa2456bf11c1
sujilgovindp <sujilgovindp@gmail.com>
2020-12-20 15:47:10 +0530

    app.js commit

---
+++ app.js
@@ -0,0 +1,46 @@
+const express = require("express");
+const morgan = require("morgan");
+const helmet = require("helmet");
+const cors = require("cors");
+const rateLimit = require("express-rate-limit");
+
+const app = express();
+
+// whitelist
+const whitelist = ['http://localhost:4001','http://localhost:4000']
+const corsOptions = {
+  origin: function (origin, callback) {
+    if (whitelist.indexOf(origin) !== -1 || !origin) {
+      callback(null, true)
+    } else {
+      callback(new Error('Not allowed by CORS'))
+    }
+  }
+}
+
+// Ratelimit
+const limiter = rateLimit({
+    windowMs: 60 * 1000, // 1 minute
+    max: 10, // limit each IP to 2 requests per windowMs
+    message: "Too many requests created from this IP, please try again after a minute"
+  });
+
+// Middleware
+app.use(morgan("common"));
+app.use(helmet());
+app.use(cors(corsOptions));
+app.use(limiter); //  apply to all requests
+
+// Port
+const port = 4001;
+
+app.get("/", (req, res) => {
+  res.json({
+    message: "Hello! How are you?",
+  });
+});
+
+// Listen
+app.listen(port, () => {
+  console.log(`Listening on port: ${port}`);
+});
